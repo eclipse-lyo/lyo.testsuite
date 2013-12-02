1eb1e37161321cc39609200ca4c705fd6a6a5b41
Samuel Padgett <spadgett@us.ibm.com>
2013-12-02 13:54:28 -0500

    Bug 422992 - Fix NullPointerException in test getResourceUsingInvalidContentType
    Add null guards to OSLCUtils.getContentType() for when entity or content
    type is null.
    
    Change-Id: I6578f60dfabf5681bd56f2fff7c95c68295743ef
    Signed-off-by: Samuel Padgett <spadgett@us.ibm.com>

--- org.eclipse.lyo.testsuite.server/src/main/java/org/eclipse/lyo/testsuite/server/util/OSLCUtils.java
+++ org.eclipse.lyo.testsuite.server/src/main/java/org/eclipse/lyo/testsuite/server/util/OSLCUtils.java
@@ -13,6 +13,7 @@
  *
  *    Steve Speicher - initial API and implementation
  *    Tim Eck II     - asset management test cases
+ *    Samuel Padgett - add null guards in getContentType()
  *******************************************************************************/
 package org.eclipse.lyo.testsuite.server.util;
 
@@ -47,6 +48,7 @@ import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathFactory;
 
 import org.apache.http.Header;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -546,7 +548,18 @@ public class OSLCUtils {
 	}
 
 	public static String getContentType(HttpResponse resp) {
-		String contentType = resp.getEntity().getContentType().getValue();
+		Assert.assertNotNull(resp);
+		HttpEntity entity = resp.getEntity();
+		if (entity == null) {
+			return null;
+		}
+		
+		Header h = entity.getContentType();
+		if (h == null) {
+			return null;
+		}
+
+		String contentType = h.getValue();
 		String contentTypeSplit[] = contentType.split(";");
 		return contentTypeSplit[0];
 	}
