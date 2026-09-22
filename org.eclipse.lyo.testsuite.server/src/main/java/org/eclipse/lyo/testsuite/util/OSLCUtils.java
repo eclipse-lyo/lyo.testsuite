1f3b9cb49b6fae9a8dfac47fb4a361d9bbbb24d1
Andrew Berezovskyi <andriib@kth.se>
2026-09-22 18:46:58 +0000

    fix: secure XML response parsing against XXE (#392)

--- org.eclipse.lyo.testsuite.server/src/main/java/org/eclipse/lyo/testsuite/util/OSLCUtils.java
+++ org.eclipse.lyo.testsuite.server/src/main/java/org/eclipse/lyo/testsuite/util/OSLCUtils.java
@@ -28,6 +28,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Scanner;
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -92,7 +93,11 @@ public class OSLCUtils {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         dbf.setNamespaceAware(true);
         dbf.setValidating(false);
-        // Don't load external DTD
+        // Response XML is untrusted. Reject DTDs and disable external entity resolution.
+        dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+        dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        dbf.setFeature("http://xml.org/sax/features/external-general-entities", false);
+        dbf.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
         dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
         DocumentBuilder db = dbf.newDocumentBuilder();
         InputSource is = new InputSource();
